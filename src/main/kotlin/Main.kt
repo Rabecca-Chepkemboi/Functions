6fb9fa1345baa30dbc56c55d05ec895abde97d95
Chepkemboi <Chepkemboirabecca@gmail.com>
2023-02-09 07:38:49 +0300

    created functions

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main() {
+printName ("Ada")
+    division()
+    sum(12, 32, 15, 24)
+    fact()
+}
+fun printName (name: String){
+    println("Hello "+ name)
+}
+fun division (){
+    var num1 = 367
+    var num2 = 12
+    println(num1/num2)
+}
+fun sum (num1: Int, num2: Int, num3: Int, num4: Int){
+    var sum = num1 + num2 +num3 +num4
+    println(sum)
+}
+fun fact (){
+    var fact = ("I love myself")
+    println(fact)
+}
